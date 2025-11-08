dd92f60323a7f0628884f5250670fc1b431b722e
Bitan56 <94443470+Bitan56@users.noreply.github.com>
2025-11-08 06:48:23 +0530

    add

--- master-script.js
+++ master-script.js
@@ -1,4 +1,4 @@
 const rollingText = document.getElementById("rolling-text")
 
 // the announcement bar
-rollingText.innerHTML = "The website has been <strong>UPGRADED !!</strong>";
+rollingText.innerHTML = "Our website has been <strong>UPGRADED !!</strong>";
