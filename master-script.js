7a12c075b179766fca50a32799d5930651754baa
Bitan56 <94443470+Bitan56@users.noreply.github.com>
2025-11-11 17:20:14 +0530

    add

--- master-script.js
+++ master-script.js
@@ -3,10 +3,3 @@ const sideBarMenu = document.getElementById("side-bar-menu-list")
 
 // the announcement bar
 rollingText.innerHTML = "Our website has been <strong>UPGRADED !!</strong>";
-
-sideBarMenu.innerHTML = `<button class="sidebar-menu-button" style="background-color: rgb(190, 189, 189);">Home</button>
-            <button class="sidebar-menu-button" onclick="location.href = 'GCETTS-TT/pages/notes.html';">Notes</button>
-            <button class="sidebar-menu-button" onclick="location.href = 'GCETTS-TT/pages/announcements.html';">Announcements</button>
-            <button class="sidebar-menu-button" onclick="location.href = 'GCETTS-TT/pages/academic-essentials.html';">Academic Essentials</button>
-            <button class="sidebar-menu-button" onclick="window.open('https://www.gcetts.ac.in/')">About our
-                College</button>`
